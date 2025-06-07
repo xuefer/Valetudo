62253dcc4dc20ec666be6491e4c5a04d64df7cb1
Xuefer <xuefer@gmail.com>
2025-06-08 00:45:51 +0800

    feat(roborock): recognize 22 as dock emptying
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- backend/lib/robots/roborock/RoborockValetudoRobot.js
+++ backend/lib/robots/roborock/RoborockValetudoRobot.js
@@ -246,6 +246,11 @@ class RoborockValetudoRobot extends MiioValetudoRobot {
         if (this.dockType === RoborockConst.DOCK_TYPE.ULTRA) {
             if (data["state"] !== undefined) {
                 switch (data["state"]) {
+                    case 22:
+                        this.state.upsertFirstMatchingAttribute(new entities.state.attributes.DockStatusStateAttribute({
+                            value: entities.state.attributes.DockStatusStateAttribute.VALUE.EMPTYING
+                        }));
+                        break;
                     case 23:
                     case 25:
                     case 26:
@@ -658,6 +663,9 @@ const STATUS_MAP = {
     18: {
         value: stateAttrs.StatusStateAttribute.VALUE.CLEANING,
         flag: stateAttrs.StatusStateAttribute.FLAG.SEGMENT
+    },
+    22: {
+        value: stateAttrs.StatusStateAttribute.VALUE.DOCKED
     },
     23: {
         value: stateAttrs.StatusStateAttribute.VALUE.DOCKED
